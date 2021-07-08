7981d06c2c6161e91d3cc5375eb751eb780b0d4b
RobFinch <5563401+robfinch@users.noreply.github.com>
2021-07-08 01:46:36 -0400

    ANY1 v3 - update
    - CC64 compiler source

---
+++ v3/software/CC64/source/glo.cpp
@@ -0,0 +1,157 @@
+// ============================================================================
+//        __
+//   \\__/ o\    (C) 2012-2021  Robert Finch, Waterloo
+//    \  __ /    All rights reserved.
+//     \/_//     robfinch<remove>@finitron.ca
+//       ||
+//
+// CC64 - 'C' derived language compiler
+//  - 64 bit CPU
+//
+// This source file is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU Lesser General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, or     
+// (at your option) any later version.                                      
+//                                                                          
+// This source file is distributed in the hope that it will be useful,      
+// but WITHOUT ANY WARRANTY; without even the implied warranty of           
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            
+// GNU General Public License for more details.                             
+//                                                                          
+// You should have received a copy of the GNU General Public License        
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.    
+//                                                                          
+// ============================================================================
+//
+#include "stdafx.h"
+/*      global definitions      */
+
+Compiler compiler;
+CPU cpu;
+int pass;
+int maxPn = 15;
+int gCpu = 7;
+int regPC = 31;
+int regSP = 30;
+int regFP = 29;
+int regLR = 1;
+int regXLR = 28;
+int regXHSP = 27;
+int regGP = 28;
+int regGP1 = 27;
+int regTP = 27;
+int regCLP = 25;                // class pointer
+int regPP = 56;					// program pointer
+int regZero = 0;
+int regFirstTemp = 4;
+int regLastTemp = 9;
+int regXoffs = 55;
+int regFirstRegvar = 10;
+int regLastRegvar = 19;
+int regFirstArg = 20;
+int regLastArg = 25;
+int regAsm = 2;
+int pregSP = 30;
+int pregFP = 29;
+int pregLR = 1;
+int pregXLR = 28;
+int pregGP = 28;
+int pregTP = 27;
+int pregCLP = 25;                // class pointer
+int pregPP = 24;					// program pointer
+int pregZero = 0;
+int pregFirstTemp = 4;
+int pregLastTemp = 9;
+int pregFirstRegvar = 11;
+int pregLastRegvar = 17;
+int pregFirstArg = 18;
+int pregLastArg = 22;
+int farcode = 0;
+int wcharSupport = 1;
+int verbose = 0;
+int use_gp = 1;
+int address_bits = 32;
+int maxVL = 64;
+int nregs = 128;
+
+int64_t sizeOfWord = 16;
+int sizeOfFP = 16;
+int sizeOfFPS = 8;
+int sizeOfFPD = 16;
+int sizeOfFPT = 24;
+int sizeOfFPQ = 32;
+int sizeOfPtr = 16;
+int sizeOfPosit = 16;
+
+std::ifstream *ifs;
+txtoStream ofs;
+txtoStream lfs;
+txtoStream dfs;
+txtoStream irfs;
+
+/*
+FILE            *input = 0,
+                *list = 0,
+                *output = 0;*/
+FILE			*outputG = 0;
+int incldepth = 0;
+int             lineno = 0;
+int             nextlabel = 1;
+int             lastch = 0;
+int             lastst = 0;
+char            lastid[128] = "";
+char            lastkw[128] = "";
+char            laststr[MAX_STRLEN + 1] = "";
+int64_t			ival = 0;
+double          rval = 0.0;
+Float128		rval128;
+Posit64 pval64;
+char float_precision = 'd';
+//FloatTriple     FAC1,FAC2;
+//FLOAT           rval = {0,0,0,0,0,0};
+int parseEsc = TRUE;
+
+TABLE           gsyms[257];// = {0,0},
+bool DataLabels[65535];
+	           
+SYM             *lasthead = (SYM *)NULL;
+Float128		*quadtab = nullptr;
+struct slit     *strtab = (struct slit *)NULL;
+struct clit		*casetab = (struct clit *)NULL;
+int             lc_static = 0;
+int             lc_auto = 0;
+int				lc_thread = 0;
+Statement    *bodyptr = 0;
+int             global_flag = 1;
+TABLE           defsyms;
+CSet *save_mask = nullptr;          /* register save mask */
+CSet *fpsave_mask = nullptr;
+CSet* psave_mask = nullptr;
+TYP             tp_int, tp_econst;
+bool dogen = true;
+int isKernel = FALSE;
+int isPascal = TRUE;
+int isOscall = FALSE;
+int isInterrupt = FALSE;
+int isTask = FALSE;
+int isNocall = FALSE;
+int optimize = TRUE;
+int opt_noregs = FALSE;
+int opt_nopeep;
+int opt_noexpr = FALSE;
+int opt_nocgo = FALSE;
+int opt_size = FALSE;
+int opt_vreg = FALSE;
+int exceptions = FALSE;
+int mixedSource = FALSE;
+Function *currentFn = (Function *)NULL;
+int callsFn = FALSE;
+int stmtdepth = 0;
+
+char nmspace[20][100];
+int bsave_mask;
+short int loop_active;
+
+ANY1CodeGenerator cg;
+
+
---
+++ v3/software/CC64/source/proto.h
@@ -0,0 +1,82 @@
+#ifndef _PROTO_H
+#define _PROTO_H
+
+TYP *forcefit(ENODE **srcnode, TYP *srctp, ENODE **dstnode, TYP *dsttp, bool promote, bool typecast);
+
+// Register.cpp
+bool IsArgumentReg(int regno);
+bool IsCalleeSave(int regno);
+
+int64_t GetConstExpression(ENODE **pnode);
+void GenMemop(int op, Operand *ap1, Operand *ap2, int ssize);
+void GenerateHint(int num);
+
+void SaveRegisterVars(CSet *rmask);
+void SaveFPRegisterVars(CSet *fprmask);
+void SavePositRegisterVars(CSet *prmask);
+void funcbottom(Statement *stmt);
+SYM *makeint2(std::string na);
+int64_t round10(int64_t n);
+int pwrof2(int64_t);
+void ListCompound(Statement *stmt);
+std::string TraceName(SYM *sp);
+void MarkRemove(OCODE *ip);
+void IRemove();
+int roundSize(TYP *tp);
+extern char *rtrim(char *);
+extern int caselit(scase *casetab, int64_t);
+extern int litlist(ENODE *);
+
+// MemoryManagement.cpp
+void FreeFunction(Function *fn);
+
+// Outcode.cpp
+extern std::streampos genstorage(int64_t nbytes);
+extern void GenerateByte(int64_t val);
+extern void GenerateChar(int64_t val);
+extern void GenerateHalf(int64_t val);
+extern void GenerateWord(int64_t val);
+extern void GenerateLong(int64_t val);
+extern void GenerateFloat(Float128 *val);
+extern void GenerateQuad(Float128 *);
+extern void GenerateReference(SYM *sp, int64_t offset);
+extern void GenerateLabelReference(int n, int64_t);
+
+extern char *RegMoniker(int regno);
+extern void push_token();
+extern void pop_token();
+extern char *GetStrConst();
+
+extern void push_typ(TYP *tp);
+extern TYP *pop_typ();
+
+extern void opt_const_unchecked(ENODE **node);
+extern Operand *MakeString(char *s);
+extern Operand *MakeDoubleIndexed(int i, int j, int scale);
+extern Operand *makecreg(int);
+
+// Register.c
+extern Operand* GetTempReg(int);
+extern Operand* GetTempRegister();
+extern Operand* GetTempTgtRegister();
+extern Operand* GetTempBrRegister();
+extern Operand* GetTempFPRegister();
+extern Operand* GetTempPositRegister();
+extern Operand* GetTempVectorRegister();
+extern Operand* GetTempVectorMaskRegister();
+extern void ReleaseTempRegister(Operand* ap);
+extern void ReleaseTempReg(Operand* ap);
+extern int TempInvalidate(int*, int*);
+extern void TempRevalidate(int sp, int fsp, int psp);
+extern int GetTempMemSpace();
+extern bool IsArgumentReg(int);
+extern Operand* GenerateFunctionCall(ENODE* node, int flags);
+
+// Utility
+extern int64_t round8(int64_t);
+extern int countLeadingBits(int64_t val);
+extern int countLeadingZeros(int64_t val);
+
+extern Posit64 GetPositExpression(ENODE** pnode);
+extern void GeneratePosit(Posit64 val);
+#endif
